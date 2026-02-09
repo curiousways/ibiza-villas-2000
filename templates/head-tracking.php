1c5f2ddf26c221ecd51a84c8b430e5c8224d42e2
David Flindall <david@curiousways.com>
2026-02-09 13:23:44 +0000

    remove double google tag being added

--- templates/head-tracking.php
+++ templates/head-tracking.php
@@ -1,14 +1,8 @@
 <meta name="facebook-domain-verification" content="lkeqvahl6otvpv3qjpvegvy5eegu2g" />
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NW8WQP42F9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-NW8WQP42F9');
-</script>
+<?php
+// Google tag G-NW8WQP42F9 is loaded by Complianz (cookie consent) - do not duplicate here.
+?>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Jost:wght@300;600&display=swap');
