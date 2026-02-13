2e636a670d3f51dae9059938a934fc80b2ff6cdc
David Flindall <71117886+curiousways@users.noreply.github.com>
2026-02-13 08:10:34 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -212,14 +212,6 @@
 	</div>
 
 	<div class="row">
-		<div class="medium-12 columns">
-			<p style="font-size: 12px; margin-bottom: 0; letter-spacing: 1px;">Babalia BV acts solely as booking agent. Accommodation services are supplied and managed by MRLP Rentals SL. Your rental contract is with MRLP Rentals SL.</p>
-			<p style="font-size: 12px; margin-bottom: 0; letter-spacing: 1px;">CENTRAL DE RESERVAS RGE CR-0120-E - COMERCIALIZADOR DE</p>
-			<p style="font-size: 12px; margin-bottom: 0; letter-spacing: 1px;">ESTANCIAS TURÍSTICAS 12752 VAT B57974438</p>
-		</div>
-	</div>
-
-	<div class="row">
 
 
 		<div class="medium-12 columns tom-whatsapp">
@@ -232,6 +224,14 @@
 			<?php wp_nav_menu( $args ); ?>
 		</div>
 	</div>
+
+	<div style="margin-top: 20px" class="row">
+		<div class="medium-12 columns">
+			<p style="font-size: 12px; margin-bottom: 0; letter-spacing: 1px;">Babalia BV acts solely as booking agent. Accommodation services are supplied and managed by MRLP Rentals SL. Your rental contract is with MRLP Rentals SL.</p>
+			<p style="font-size: 12px; margin-bottom: 0; letter-spacing: 1px;">CENTRAL DE RESERVAS RGE CR-0120-E - COMERCIALIZADOR DE</p>
+			<p style="font-size: 12px; margin-bottom: 0; letter-spacing: 1px;">ESTANCIAS TURÍSTICAS 12752 VAT B57974438</p>
+		</div>
+	</div>		
 
 
 
